1dad603a08f1bf05e7c4a9f214d18bc3a1ede265
nori <nori623p@gmail.com>
2021-11-23 08:51:43 +0100

    ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,10 +1,14 @@
 #include <stdlib.h>
 
 #include <Array.hpp>
+#include <iomanip>
 #include <iostream>
 
+#include "Fixed.hpp"
+#define GREEN_BOLD "\x1b[32;1m"
+#define RESET_COLOR "\x1b[39;0m"
 #define MAX_VAL 750
-int main(int, char**) {
+void example() {
   Array<int> numbers(MAX_VAL);
   int* mirror = new int[MAX_VAL];
   srand(time(NULL));
@@ -21,7 +25,7 @@ int main(int, char**) {
   for (int i = 0; i < MAX_VAL; i++) {
     if (mirror[i] != numbers[i]) {
       std::cerr << "didn't save the same value!!" << std::endl;
-      return 1;
+      return;
     }
   }
   try {
@@ -39,5 +43,92 @@ int main(int, char**) {
     numbers[i] = rand();
   }
   delete[] mirror;  //
-  return 0;
+  return;
+}
+
+template <typename U, typename T>
+void setval(Array<T>& ary, unsigned int size) {
+  for (unsigned int i = 0; i < size; i++) {
+    ary[i] = T((U)('a' + i));
+  }
+}
+
+template <typename T>
+void print(Array<T>& ary, unsigned int size) {
+  for (unsigned int i = 0; i < size; i++) {
+    std::cout << ary[i] << ", ";
+  }
+  std::cout << std::endl;
+}
+
+template <typename T>
+void sizetest(const unsigned int size) {
+  std::cout << "\n======= size test =======\n" << std::endl;
+
+  Array<T> f1;
+  Array<T> f2(size);
+
+  std::cout << "default size : " << f1.size() << std::endl;
+  std::cout << std::left << std::setw(10) << "param " << f2.size() << " : "
+            << f2.size() << std::endl;
+}
+
+template <typename T, typename U>
+void copytest(const unsigned int size) {
+  std::cout << "\n======= copy test =======\n" << std::endl;
+
+  Array<T> src(size);
+  setval<U>(src, size);
+  Array<T> dest(src);
+
+  std::cout << std::left << std::setw(13) << "src"
+            << ": ";
+  print(src, size);
+  src = dest;
+  std::cout << std::left << std::setw(13) << "dest"
+            << ": ";
+  print(dest, size);
+}
+
+template <typename T, typename U>
+void eqtest(const unsigned int size) {
+  std::cout << "\n======== eq test ========\n" << std::endl;
+
+  Array<T> f1;
+  Array<T> f2(size);
+
+  setval<U>(f2, size);
+  std::cout << std::left << std::setw(13) << "value"
+            << ": ";
+  print(f2, size);
+  f1 = f2;
+  std::cout << std::left << std::setw(13) << "assign"
+            << ": ";
+  print(f1, size);
+}
+template <typename T, typename U>
+void excepttest(const unsigned int size, U func) {
+  std::cout << "\n====== except test ======\n" << std::endl;
+  try {
+    Array<T> f1(size);
+    func(f1, size + 1);
+  } catch (std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
+}
+
+template <typename T, typename U>
+void test(const unsigned int size) {
+  std::cout << GREEN_BOLD "\n==========TEST===========" RESET_COLOR
+            << std::endl;
+  sizetest<T>(size);
+  copytest<T, U>(size);
+  eqtest<T, U>(size);
+}
+
+int main(int, char**) {
+  example();
+  test<Fixed, int>(10);
+  test<int, int>(10);
+  test<char, char>(10);
 }
